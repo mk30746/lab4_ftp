855b29b2625bab38623d50e05dc8458b4a9b9d87
mk30746 <44784760+mk30746@users.noreply.github.com>
2019-01-15 18:38:02 +0100

    initial commit

---
+++ ftp/ftp/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ftp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_adres = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_folder = new System.Windows.Forms.TextBox();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_adres
+            // 
+            this.textBox_adres.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_adres.Location = new System.Drawing.Point(103, 39);
+            this.textBox_adres.Name = "textBox_adres";
+            this.textBox_adres.Size = new System.Drawing.Size(365, 22);
+            this.textBox_adres.TabIndex = 1;
+            this.textBox_adres.Text = "ftp://testlabsieciowe.cba.pl";
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(85, 177);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(128, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_log.Location = new System.Drawing.Point(33, 206);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_log.Size = new System.Drawing.Size(435, 215);
+            this.textBox_log.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "password";
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_login.Location = new System.Drawing.Point(103, 121);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(365, 22);
+            this.textBox_login.TabIndex = 1;
+            this.textBox_login.Text = "testlabsieciowe";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_password.Location = new System.Drawing.Point(103, 149);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(365, 22);
+            this.textBox_password.TabIndex = 1;
+            this.textBox_password.Text = "Haslo123";
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_port.Location = new System.Drawing.Point(103, 93);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(365, 22);
+            this.textBox_port.TabIndex = 1;
+            this.textBox_port.Text = "21";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "folder";
+            // 
+            // textBox_folder
+            // 
+            this.textBox_folder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_folder.Location = new System.Drawing.Point(103, 67);
+            this.textBox_folder.Name = "textBox_folder";
+            this.textBox_folder.Size = new System.Drawing.Size(365, 22);
+            this.textBox_folder.TabIndex = 1;
+            this.textBox_folder.Text = "/testlabsieciowe.cba.pl/";
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(274, 177);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(128, 23);
+            this.button_clear.TabIndex = 2;
+            this.button_clear.Text = "Clear log";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 450);
+            this.Controls.Add(this.textBox_log);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_login);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_folder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_adres);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FTP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox_adres;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_folder;
+        private System.Windows.Forms.Button button_clear;
+    }
+}
+
---
+++ ftp/ftp/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace ftp
+{
+    public partial class Form1 : Form
+    {
+        public static string error;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class ftp
+        {
+            private string host = null;
+            private string user = null;
+            private string pass = null;
+            private FtpWebRequest ftpRequest = null;
+            private FtpWebResponse ftpResponse = null;
+            private Stream ftpStream = null;
+
+            public ftp(string host_in, string userName, string password)
+            {
+                host = host_in;
+                user = userName;
+                pass = password;
+            }
+
+            public string[] directoryListDetailed()
+            {
+                try
+                {
+                    ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host);
+                    ftpRequest.Credentials = new NetworkCredential(user, pass);
+                    ftpRequest.UseBinary = true;
+                    ftpRequest.UsePassive = true;
+                    ftpRequest.KeepAlive = true;
+                    ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+                    ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                    ftpStream = ftpResponse.GetResponseStream();
+                    StreamReader ftpReader = new StreamReader(ftpStream);
+                    string directoryRaw = null;
+                    try { while (ftpReader.Peek() != -1) { directoryRaw += ftpReader.ReadLine() + "|"; } }
+                    catch (Exception ex) { error = ex.ToString(); }
+                    ftpReader.Close();
+                    ftpStream.Close();
+                    ftpResponse.Close();
+                    ftpRequest = null;
+                    try { string[] directoryList = directoryRaw.Split("|".ToCharArray()); return directoryList; }
+                    catch (Exception ex) { error = ex.ToString(); }
+                }
+                catch (Exception ex) { error=ex.ToString(); }
+                return new string[] { error };
+            }
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            string adres = textBox_adres.Text+":"+textBox_port.Text+textBox_folder.Text;
+            string login = textBox_login.Text;
+            string haslo = textBox_password.Text;
+
+            ftp ftpClient = new ftp(adres, login, haslo);
+
+            string[] detailDirectoryListing = ftpClient.directoryListDetailed();
+            for (int i = 0; i < detailDirectoryListing.Count(); i++)
+            {
+                textBox_log.AppendText(detailDirectoryListing[i]);
+                textBox_log.AppendText("\r\n");
+            }
+            ftpClient = null;
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            textBox_log.Clear();
+        }
+    }
+}
